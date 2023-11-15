973300cfa35111297f61cde00fbb57f9f7f4885b
amanavi75 <amanavi75@gmail.com>
2023-11-16 00:33:54 +0530

    created my first component

---
+++ basic react app/src/title.jsx
@@ -0,0 +1,9 @@
+function Title() {
+    return <h1> I am the title !</h1>;
+  }
+  //* creating our first  component 
+  //* justing creating a component is not enough even we have to render the component 
+
+
+  //* after that we have to export it so that it can be imported by app.jsx
+export default Title;
