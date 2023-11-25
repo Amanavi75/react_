06fa06a625f474de00c99302bbd127e4372392aa
amanavi75 <amanavi75@gmail.com>
2023-11-25 23:35:00 +0530

    done with adding the new task using set function

--- todo/src/TodoList.jsx
+++ todo/src/TodoList.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react"
 export default function TodoList(){
     let [Todos,setTodos] = useState(["sample Task"]);
-    //* initializing the useState empty array 
+    let [newTodo, setNewTodo] = useState("")
+    //* initializing the useState  empty array 
+
+    let addNewTask= () =>{
+        setTodos([...Todos, newTodo]);
+    }
+
+    let updateTodoValue= (event) => {
+    setNewTodo(event.target.value)
+    };
+
+    // this function is use to trigger the update in the todo values 
+    
+
+
+    //* here onchange is used to trigger the change , whenever there will be any change in the todovalue this change will trigger 
+
     return (
         <div>
-            <input placeholder='add your task'></input>
+            <input placeholder='add your task' 
+            value={newTodo} onChange={updateTodoValue}></input>
             <br></br>      
-     <button>add task</button>
+     <button onClick={addNewTask}>add task</button>
      <br></br>
      <br></br>
      <br></br>
