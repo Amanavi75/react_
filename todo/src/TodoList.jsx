21c3b3ef21abd2b0cc42ddcedc625c0eaf0ff5ca
amanavi75 <amanavi75@gmail.com>
2023-11-25 23:17:03 +0530

    create an state array

--- todo/src/TodoList.jsx
+++ todo/src/TodoList.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
 export default function TodoList(){
+    let [Todos,setTodos] = useState(["sample Task"]);
+    //* initializing the useState empty array 
     return (
         <div>
-           <h4>TodoList</h4> 
-        </div>
+            <input placeholder='add your task'></input>
+            <br></br>      
+     <button>add task</button>
+     <br></br>
+     <br></br>
+     <br></br>
+     <h4>TodoList</h4> 
+     <ul>
+        {
+           Todos.map((todo)=>(
+             <li>{todo}</li>
+           ))
+           // in case if we  use the normal parenthesis then we dont have wirte the return statement but if we use curly braces then we have write the return keyboard 
+        }
+     </ul>
+    </div>
     )
 }
