acdbb53b6bcc5f195db59f1960cb833cbe47b40c
amanavi75 <amanavi75@gmail.com>
2023-11-29 01:31:14 +0530

    done with functionality

--- reactform/src/CommentsForm.jsx
+++ reactform/src/CommentsForm.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react"
+
 export default function CommentsForm (){
+
+    let[formData,setformData] = useState({
+        username: "",
+        remarks: "",
+        rating: 5,
+    })
+
+    // creating state variable as object 
+    
+
+    let handleInputChange = (event) =>{
+        setformData((currData)=>{
+            return {...currData,[event.target.name] : event.target.value}  // re rendering the ui 
+        });
+    }
+    // input handler for multiple inputs 
+    
+    let handleSubmit = (event) => {
+       console.log(formData)
+       event.preventDefault();
+    }
+    // preventing the default criteria for the form 
+
     return(
         <div>
             <h4> Give a comment</h4>
-            <form>
-                <input placeholder="username" type="text" />
+            <form onSubmit={handleSubmit}> 
+            <label htmlFor="username">username</label>
+                <input placeholder="username" type="text" value={formData.username} onChange={handleInputChange} id="username" name="username"/>
                 <br /> <br /> 
-                <textarea >Remarks </textarea>
+                <label htmlFor="remarks">remarks</label>
+                <textarea  value={formData.remarks} placeholder="remarks" onChange={handleInputChange} id="remarks" name="remarks">Remarks </textarea>
                 <br /> <br /> 
-                <input placeholder="rating" type="number" min={1} max={5}/>
+                <label htmlFor="rating">rating</label>
+                <input placeholder="rating" type="number" min={1} max={5} value={formData.rating} onChange={handleInputChange} id="rating" name="rating"/>
                 <br /> <br /> 
                 <button>add commnet</button>
             </form>
